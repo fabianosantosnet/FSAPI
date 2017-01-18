da144783abfa9f556539c1297cb1c28b72aff47e
fabianosantosnet <fabiano@fabianosantos.net>
2017-01-18 15:07:14 -0200

    Update api.php - add GET method - corrected comments

--- api.php
+++ api.php
@@ -1,9 +1,9 @@
 <?php
 #header('Content-Type: text/html; charset=utf-8');
 header('Content-Type:application/json;charset=utf-8');
-$hostname = "localhost";    // Vamos considerar localhost ou máquina local
-$username = "root";    // Username é userTeste
-$password = "root";     // Password é passTeste (Cuidado com maiúsculas e minúsculas
+$hostname = "localhost"; // Vamos considerar localhost ou máquina local
+$username = "root";      // Username do banco mysql
+$password = "root";      // Password do banco mysql
 $_URL_COMPLETE='http://'.$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME'];
 
 $c=mysql_connect($hostname, $username, $password) or die(mysql_error());
