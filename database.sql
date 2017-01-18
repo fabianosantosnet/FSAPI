a1e209e8669db5591182c900c1bff8f5b89f5a31
fabianosantosnet <fabiano@fabianosantos.net>
2017-01-18 15:15:05 -0200

    Add files: database.sql (use it to create the database structure) / DEINFO_AB_FEIRASLIVRES_2014.csv this CSV is the source of project load in the database using database.sql

---
+++ database.sql
@@ -0,0 +1,29 @@
+create database feiras;
+
+use feiras;
+
+create table cadastrofeiras
+(
+  ID int not null,
+  LONGITUDE int not null,
+  LATITUDE int not null,
+  SETCENS int not null,
+  AREAP int not null,
+  CODDIST int not null,
+  DISTRITO varchar(255) not null,
+  CODSUBPREF int not null,
+  SUBPREFE varchar(255) not null,
+  REGIAO5 varchar(255) not null,
+  REGIAO8 varchar(255) not null,
+  NOME_FEIRA varchar(255) not null,
+  REGISTRO varchar(255) not null,
+  LOGRADOURO varchar(255) not null,
+  NUMERO varchar(255) not null,
+  BAIRRO varchar(255) not null,
+  REFERENCIA varchar(255) not null,
+  primary key (ID)
+);
+
+
+# in the prompt line 'mysql>' execute the code. Change the path.
+LOAD DATA LOCAL INFILE '/home/usuario/Documents/DEINFO_AB_FEIRASLIVRES_2014.csv' INTO TABLE cadastrofeiras FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n';
