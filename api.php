da3702bedac8ad60af050b0119d41bfc03b9f155
fabianosantosnet <fabiano@fabianosantos.net>
2017-01-18 15:04:57 -0200

    Update api.php - add GET method

--- api.php
+++ api.php
@@ -1,9 +1,9 @@
 <?php
+#header('Content-Type: text/html; charset=utf-8');
 header('Content-Type:application/json;charset=utf-8');
 $hostname = "localhost";    // Vamos considerar localhost ou máquina local
-$username = "root";         // Username do banco mysql
-$password = "root";         // Password do banco mysql
-
+$username = "root";    // Username é userTeste
+$password = "root";     // Password é passTeste (Cuidado com maiúsculas e minúsculas
 $_URL_COMPLETE='http://'.$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME'];
 
 $c=mysql_connect($hostname, $username, $password) or die(mysql_error());
@@ -12,6 +12,42 @@ $c=mysql_connect($hostname, $username, $password) or die(mysql_error());
 $e=mysql_select_db("feiras") or die(mysql_error());
 #echo "Base de dados seleccionada!<br/>";
 
+#echo basename($_URL_COMPLETE);
+$url=explode('/',$_SERVER['PHP_SELF']);
+if(count($url)<5)
+{
+    http_response_code(400);
+    echo '400 BAD REQUEST';
+    exit;
+}
 
+switch($_SERVER['REQUEST_METHOD'])
+{
+    case 'GET': 
+            $get = &$_GET;
+            #ID	LONG	LAT	SETCENS	AREAP	CODDIST	DISTRITO	CODSUBPREF	SUBPREFE	REGIAO5	REGIAO8	NOME_FEIRA	REGISTRO	LOGRADOURO	NUMERO	BAIRRO	REFERENCIA
+            switch(strtolower($url[3]))
+            {
+                case 'v1':
+                    switch(strtolower($url[4]))
+                    {
+                        case 'listarfeiras':
+                            require_once('get/listarfeiras.php');
+                        break;
+
+                        default:
+                            http_response_code(405);
+                            echo '405 METHOD NOT ALLOWED';
+                            exit;
+                        break;                                                
+                    }
+                break;
+
+    default:
+            http_response_code(405);
+            echo '405 METHOD NOT ALLOWED';
+            exit;
+        break;
+}
 mysql_close();
 ?>
